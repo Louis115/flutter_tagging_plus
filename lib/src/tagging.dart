3f1f03321c588ceae90bd54edee5da33e5909182
Louis Wong <154124680+Louis115@users.noreply.github.com>
2025-06-29 04:36:30 +0800

    remove dispose of the FocusNode

--- lib/src/tagging.dart
+++ lib/src/tagging.dart
@@ -196,7 +196,7 @@ class _FlutterTaggingState<T extends Taggable>
   @override
   void dispose() {
     _textController.dispose();
-    _focusNode.dispose();
+    // _focusNode.dispose(); // dispose outside
     super.dispose();
   }
 
